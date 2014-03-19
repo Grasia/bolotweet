18494833191eb82b35e2862449db4bf7923de04c
Alvaro <alvorteg@ucm.es>
2014-03-19 17:44:10 +0100

    Creados plugins para hacer un Backup y un Restore de toda la página.

---
+++ scripts/backupBT.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+# Almacenamos la ruta actual
+path=`pwd`
+
+printf "Este script necesita ejecutar algunos comandos como SuperUsuario.\n"
+read -p "Presiona [Enter] para empezar el BackUp..."
+
+# Paramos Apache para evitar copias corruptas.
+sudo -p "Introduce tu contraseña: " service apache2 stop > /dev/null 2>&1
+printf "Parando Apache..."
+if [ $? -eq 0 ]; then
+printf "Listo!\n"
+else
+printf "Fallo. Abortando...\n"
+exit 1
+fi
+
+timeStamp=$(date '+%F')
+ext=".tar.gz"
+
+# Creamos el nombre del contenedor final
+nameFolder="backup_"$timeStamp$ext
+
+# Creamos el nombre del contenedor de los archivos de la web.
+nameFile="web_files.tar.gz";
+
+# Creamos el nombre del archivo con la BBDD.
+nameSql="bbdd.sql";
+
+# Creamos la copia de la carpeta /var/www/ entera.
+
+cd /var/
+
+printf "Creando Backup de /var/www/..."
+tar -Pzcf ~/$nameFile --exclude='.git' www/
+if [ $? -eq 0 ]; then
+printf "Listo!\n"
+else
+printf "Fallo. Abortando...\n"
+exit 1
+fi
+
+printf "Creando Backup de la Base de Datos..."
+mysqldump -u adminbtdb -pbtv2db! bolotweetdb > ~/$nameSql
+if [ $? -eq 0 ]; then
+printf "Listo!\n"
+else
+printf "Fallo. Abortando...\n"
+exit 1
+fi
+
+cd ~
+
+printf "Creando Fichero Final..."
+tar -Pzcf ~/$nameFolder {"$nameFile","$nameSql"}
+if [ $? -eq 0 ]; then
+printf "Listo!\n"
+else
+printf "Fallo. Abortando...\n"
+exit 1
+fi
+
+printf "Borrando archivos temporales..."
+rm ~/$nameFile ~/$nameSql
+if [ $? -eq 0 ]; then
+printf "Listo!\n"
+else
+printf "Fallo. Borre los ficheros manualmente en home.\n"
+fi;
+
+# Rearrancamos los servicios parados
+printf "Iniciando Apache...\n"
+sudo service apache2 start > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+printf "Listo!\n"
+else
+printf "Fallo. Intente iniciar Apache manualmente.\n"
+fi;
+
+cd $path
+
+printf "BackUp terminado\n\n"
+read -p "Presiona [Enter] para terminar..."
+
+
---
+++ scripts/restoreBT.sh
@@ -0,0 +1,147 @@
+#!/bin/bash
+
+# Almacenamos la ruta actual
+path=`pwd`
+
+if [ -z "$1" ];  then
+printf "No se ha indicado el nombre del fichero de BackUp\n"
+printf "Abortando...\n"
+exit 1
+fi
+
+printf "Este script necesita ejecutar algunos comandos como SuperUsuario.\n"
+read -p "Presiona [Enter] para empezar la restauración..."
+
+# Paramos Apache para evitar copias corruptas.
+sudo -p "Introduce tu contraseña: " service apache2 stop > /dev/null 2>&1
+printf "Parando Apache..."
+if [ $? -eq 0 ]; then
+printf "Listo!\n"
+else
+printf "Fallo. Abortando...\n"
+exit 1
+fi
+sudo service apache2 stop > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+printf "Listo!\n"
+else
+printf "Fallo. Abortando...\n"
+exit 1
+fi
+
+# Nombre del contenedor de los archivos de la web.
+nameFile="web_files.tar.gz";
+
+# Nombre del archivo con la BBDD.
+nameSql="bbdd.sql";
+
+# Nos movemos donde está el archivo de BackUp
+cd ~
+
+# Extraemos los archivos.
+
+if [ -e "$1" ]; then
+printf "Extrayendo fichero BackUp..."
+tar -xzf $1
+if [ $? -eq 0 ]; then
+printf "Listo!\n"
+else
+printf "Fallo. Abortando...\n"
+exit 1
+fi
+else
+printf "Fichero de BackUp inexistente.\n"
+printf "Abortando...\n"
+exit 1
+fi
+
+if [ -e "$nameFile" ]; then
+printf "Extrayendo fichero secundario..."
+tar -xzf $nameFile
+if [ $? -eq 0 ]; then
+printf "Listo!\n"
+else
+printf "Fallo. Abortando...\n"
+exit 1
+fi
+else
+printf "Error con el archivo de BackUp.\n"
+printf "Revisa que no esté corrupto\n"
+printf "Abortando...\n"
+exit 1
+fi
+
+# Borramos la carpeta /var/www/ si es que tiene contenido.
+printf "Borrando contenido de carpeta /var/www/..."
+sudo rm -r /var/www/* > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+printf "Listo!\n"
+else
+printf "Fallo. Ya estaba vacío.\n"
+fi
+# Copiamos el nuevo contenido
+printf "Copiando nuevo contenido a carpeta /var/www/..."
+sudo cp -R www/* /var/www/
+if [ $? -eq 0 ]; then
+printf "Listo!\n"
+else
+printf "Fallo. Abortando...\n"
+exit 1
+fi
+
+
+# Borramos la database por si existe.
+printf "Borrando database antigua..."
+mysqladmin -uadminbtdb -pbtv2db! -f drop bolotweetdb 2>/dev/null
+
+if [ $? -eq 0 ]; then
+printf "Listo!\n"
+else
+printf "Fallo. Ya estaba borrada.\n"
+fi;
+
+# Creamos la nueva database.
+printf "Creando database nueva..."
+mysqladmin -u "uadminbtdb" --password="btv2db!" create bolotweetdb
+
+if [ $? -eq 0 ]; then
+printf "Listo!\n"
+else
+printf "Fallo. Abortando...\n"
+exit 1;
+fi;
+
+
+# Restauramos el BackUp de la BBDD
+printf "Restaurando Backup de la Base de Datos..."
+mysql -u uadminbtdb -pbtv2db! bolotweetdb < $nameSql
+
+if [ $? -eq 0 ]; then
+printf "Listo!\n"
+else
+printf "Fallo. Abortando...\n"
+exit 1;
+fi;
+
+printf "Borrando archivos temporales..."
+rm -R www/ bbdd.sql web_files.tar.gz
+if [ $? -eq 0 ]; then
+printf "Listo!\n"
+else
+printf "Fallo. Borre los ficheros manualmente en home.\n"
+fi;
+
+# Rearrancamos los servicios parados
+printf "Iniciando Apache...\n"
+sudo service apache2 start > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+printf "Listo!\n"
+else
+printf "Fallo. Intente iniciar Apache manualmente.\n"
+fi;
+
+cd $path
+
+printf "Restauración terminada\n\n"
+read -p "Presiona [Enter] para terminar..."
+
