64e1c388bb78366d5c2530af67fae86844eeda00
Alvaro <alvorteg@ucm.es>
2014-03-30 11:29:22 +0200

    Cambiado NotesPDFPlugin. Ahora el submenu "Apuntes" se crea utilizando el nuevo hook onStartToolsLocalNav.

--- local/plugins/NotesPDF/NotesPDFPlugin.php
+++ local/plugins/NotesPDF/NotesPDFPlugin.php
@@ -53,19 +53,14 @@ class NotesPDFPlugin extends Plugin {
         return true;
     }
 
-    function onStartDefaultLocalNav($action) {
+    function onStartToolsLocalNav($action) {
         // '''common_local_url()''' gets the correct URL for the action name we provide
         
         $actionName = $action->trimmed('action');
         
         $user = common_current_user();
         if (!empty($user)) {
-            $action->out->elementStart('li');
-            $action->out->element('h3', null, 'Herramientas');
-            $action->out->elementStart('ul', array('class' => 'nav'));
             $action->menuItem(common_local_url('notesgroups'), _m('Apuntes'), _m('Apuntes en PDF'), ($actionName === 'notesgroups' || $actionName === 'notescustomize'), 'nav_notespdf');
-            $action->out->elementEnd('ul');
-            $action->out->elementEnd('li');
         }
 
         return true;
