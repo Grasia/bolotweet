b716f4fa1af403ef43bb088cde74c255eb337d6c
Alvaro <alvorteg@ucm.es>
2014-04-24 11:51:52 +0200

    Creado javascript que controla mediante Ajax los combobox de apuntes personalizados, de manera que al modificar uno, se actualicen el resto.

--- local/plugins/NotesPDF/js/notespdf.js
+++ local/plugins/NotesPDF/js/notespdf.js
@@ -1,3 +1,78 @@
-$('.notes-combo-manual').on('click', function() {
-  alert('Hola'); 
-});
+function changeComboTag(groupid) {
+
+    var tag = $('#notes-combo-hashtag').find(":selected").text();
+    var userid = $('#notes-combo-user').find(":selected").text();
+    var grade = $('#notes-combo-grade').find(":selected").text();
+
+    $('#notes-combo-user').load('/bolotweet/local/plugins/NotesPDF/scripts/updateBoxUser.php', {groupid: groupid, grade: grade, tag: tag}, function() {
+
+        if (userid !== 'Todos') {
+            $("#notes-combo-user option[value=" + userid + "]").attr("selected", "selected");
+        }
+
+    });
+
+    $('#notes-combo-grade').load('/bolotweet/local/plugins/NotesPDF/scripts/updateBoxGrade.php', {groupid: groupid, userid: userid, tag: tag}, function() {
+
+        if (grade !== 'Todos') {
+            $("#notes-combo-grade option[value=" + grade + "]").attr("selected", "selected");
+        }
+    });
+
+
+}
+
+function changeComboUser(groupid) {
+
+    var tag = $('#notes-combo-hashtag').find(":selected").text();
+    var userid = $('#notes-combo-user').find(":selected").text();
+    var grade = $('#notes-combo-grade').find(":selected").text();
+
+    $('#notes-combo-hashtag').load('/bolotweet/local/plugins/NotesPDF/scripts/updateBoxTags.php', {groupid: groupid, userid: userid, grade: grade}, function() {
+
+        if (tag !== 'Todos') {
+            $("#notes-combo-hashtag option[value=" + tag + "]").attr("selected", "selected");
+        }
+    });
+
+
+
+    $('#notes-combo-grade').load('/bolotweet/local/plugins/NotesPDF/scripts/updateBoxGrade.php', {groupid: groupid, userid: userid, tag: tag}, function() {
+
+        if (grade !== 'Todos') {
+            $("#notes-combo-grade option[value=" + grade + "]").attr("selected", "selected");
+            //alert();
+        }
+    });
+
+
+}
+
+function changeComboGrade(groupid) {
+
+    var tag = $('#notes-combo-hashtag').find(":selected").text();
+    var userid = $('#notes-combo-user').find(":selected").text();
+    var grade = $('#notes-combo-grade').find(":selected").text();
+
+    $('#notes-combo-user').load('/bolotweet/local/plugins/NotesPDF/scripts/updateBoxUser.php', {groupid: groupid, grade: grade, tag: tag}, function() {
+       
+        if (userid !== 'Todos') {
+            $("#notes-combo-user option[value=" + userid + "]").attr("selected", "selected");
+        }
+    });
+
+
+    $('#notes-combo-hashtag').load('/bolotweet/local/plugins/NotesPDF/scripts/updateBoxTags.php', {groupid: groupid, userid: userid, grade: grade}, function() {
+
+        if (tag !== 'Todos') {
+            $("#notes-combo-hashtag option[value=" + tag + "]").attr("selected", "selected");
+        }
+    });
+
+
+}
+
+
+
+
+
